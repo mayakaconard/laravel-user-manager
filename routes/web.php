4d33f4ab4e9bb65e040a421ea626d700a7de54e0
mayakaconard <mayakaconard@gmail.com>
2020-07-15 10:43:00 +0300

    Project Setup

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/roles', 'PermissionController@Permission');
+
+
+Route::group(['middleware' => 'role:developer'], function () {
+
+    Route::get('/admin', function () {
+
+        return 'Welcome Admin';
+    });
+});
